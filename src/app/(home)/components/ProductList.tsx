236d861b3ab55ad72fb5357731a0d376a20df176
Amanda Ramirez <amandadeog@gmail.com>
2023-12-05 09:27:43 -0300

    feat: add heading for product list

--- src/app/(home)/components/ProductList.tsx
+++ src/app/(home)/components/ProductList.tsx
@@ -8,13 +8,16 @@ interface ProductListProps {
 const ProductList = ({products}: ProductListProps) => {
     
     return ( 
-        <div className="flex w-full gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden pb-8">
-            {
-                products.map(product => (
-                    <ProductItem key={product.id} product={ComputedProductTotalPrice(product)} />
-                ))
-            }
-        </div>
+        <>
+            <p className="font-semibold uppercase mb-2">Ofertas</p>
+            <div className="flex w-full gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden pb-8">
+                {
+                    products.map(product => (
+                        <ProductItem key={product.id} product={ComputedProductTotalPrice(product)} />
+                    ))
+                }
+            </div>
+        </>
      );
 }
  
