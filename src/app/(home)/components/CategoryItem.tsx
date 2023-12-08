597042e72c6ba8e731053d30b08c23c9e2d507dc
Amanda Ramirez <amandadeog@gmail.com>
2023-12-08 17:05:45 -0300

    feat: add link to category page in CategoryItem component

--- src/app/(home)/components/CategoryItem.tsx
+++ src/app/(home)/components/CategoryItem.tsx
@@ -1,22 +1,25 @@
 import { CATEGORY_ICON } from "@/app/constants/categoryIcons";
 import { Badge } from "@/components/ui/badge";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 
 interface CategoryItemProps {
     category: Category;
 }
 
 const CategoryItem = ({category}: CategoryItemProps) => {
-    return ( <div>
-        <Badge variant={"outline"} className="category px-8 py-3 flex gap-2 justify-center items-center rounded-lg hover:cursor-pointer hover:bg-primary">
+    return (
+        <Link href={`/category/${category.slug}`}>
+            <Badge variant={"outline"} className="category px-8 py-3 flex gap-2 justify-center items-center rounded-lg hover:cursor-pointer hover:bg-primary">
             {
                 CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]
             }
-            <span className="text-xs font-bold">
-                {category.name}
-            </span>
-        </Badge>
-    </div> );
+                <span className="text-xs font-bold">
+                    {category.name}
+                </span>
+            </Badge>
+        </Link>
+    )
 }
  
 export default CategoryItem;
