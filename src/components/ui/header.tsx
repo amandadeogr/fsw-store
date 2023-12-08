8efcf4e58753e9fbb03d1f3efc2fbf0a0fc40810
Amanda Ramirez <amandadeog@gmail.com>
2023-12-08 17:07:18 -0300

    feat: add a link to home page when the user clicks on the FSW store logo

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,10 @@
 import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 import Authenticator from "../authenticator";
 import Profile from "../profile";
+import Link from "next/link";
 
 const Header = () => {
 
@@ -26,22 +27,31 @@ const Header = () => {
                     <div className="mt-2 flex flex-col gap-2">
                         <Authenticator />
                         
-                        <Button variant={"outline"} className="flex gap-2 w-full justify-start">
-                            <HomeIcon size={16} /> Início
-                        </Button>
-
-                        <Button variant={"outline"} className="flex gap-2 w-full justify-start">
-                            <PercentIcon size={16} /> Ofertas
-                        </Button>
-
-                        <Button variant={"outline"} className="flex gap-2 w-full justify-start">
-                            <ListOrderedIcon size={16} /> Catálogo
-                        </Button>
+                        <SheetClose asChild>
+                            <Link href="/">
+                                <Button variant={"outline"} className="flex gap-2 w-full justify-start">
+                                    <HomeIcon size={16} /> Início
+                                </Button>
+                            </Link>
+                        </SheetClose>
+                        <SheetClose asChild>
+                            <Button variant={"outline"} className="flex gap-2 w-full justify-start">
+                                <PercentIcon size={16} /> Ofertas
+                            </Button>
+                        </SheetClose>
+                        <SheetClose asChild>
+                            <Link href="/catalog">
+                                <Button variant={"outline"} className="flex gap-2 w-full justify-start">
+                                    <ListOrderedIcon size={16} /> Catálogo
+                                </Button>
+                            </Link>
+                        </SheetClose>
                     </div>
                 </SheetContent>
             </Sheet>
-
-            <h1 className="font-semibold text-lg"><span className="text-primary">FSW</span> Store</h1>
+            <Link href="/">
+                <h1 className="font-semibold text-lg"><span className="text-primary">FSW</span> Store</h1>
+            </Link>
 
             <Button variant={"outline"} size={"default"}>
                 <ShoppingCartIcon />
