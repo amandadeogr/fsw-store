623bf1f82dd48e6f95aec92308aae0523b2f731d
Amanda Ramirez <amandadeog@gmail.com>
2023-12-08 17:05:55 -0300

    fix: update category icon sizes

--- src/app/constants/categoryIcons.tsx
+++ src/app/constants/categoryIcons.tsx
@@ -1,10 +1,10 @@
 import { HeadphonesIcon, KeyboardIcon, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react"
 
 export const CATEGORY_ICON = {
-    keyboards: <KeyboardIcon  size={"24px"}/>,
-    monitors: <MonitorIcon  size={"24px"}/>,
-    mouses: <MouseIcon  size={"24px"}/>,
-    speakers: <SpeakerIcon  size={"24px"}/>,
-    mousepads: <SquareIcon  size={"24px"}/>,
-    headphones: <HeadphonesIcon size={"24px"}/>,
+    keyboards: <KeyboardIcon  size={"20px"}/>,
+    monitors: <MonitorIcon  size={"20px"}/>,
+    mouses: <MouseIcon  size={"20px"}/>,
+    speakers: <SpeakerIcon  size={"20px"}/>,
+    mousepads: <SquareIcon  size={"20px"}/>,
+    headphones: <HeadphonesIcon size={"20px"}/>,
 }
