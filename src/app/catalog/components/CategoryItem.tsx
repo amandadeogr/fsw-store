0e776f9f57b0ca8c3b853a66d0baa90baf3876c6
Amanda Ramirez <amandadeog@gmail.com>
2023-12-08 17:50:08 -0300

    style: refactor category item component styles

--- src/app/catalog/components/CategoryItem.tsx
+++ src/app/catalog/components/CategoryItem.tsx
@@ -9,24 +9,24 @@ interface CategoryItemsProps {
 const CategoryItem = ({category}:CategoryItemsProps) => {
     return ( 
         <Link href={`/category/${category.slug}`}>
-            <div className="flex flex-col w-[190px] h-[230px] hover:cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out">
-            <div className="bg-category-gradient flex justify-center items-center h-[190px] w-full rounded-t-xl">
-                <Image 
-                    src={category.imageUrl} 
-                    alt={category.name} 
-                    width={0} 
-                    height={0} 
-                    sizes="100vw"
-                    className="h-auto w-[110px]"
-                    style={{
-                        objectFit: 'contain'
-                    }}
-                />
+            <div className="flex flex-col w-[170px] sm:w-[190px] h-[230px] hover:cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out">
+                <div className="bg-category-gradient flex justify-center items-center h-[190px] w-full rounded-t-xl">
+                    <Image 
+                        src={category.imageUrl} 
+                        alt={category.name} 
+                        width={0} 
+                        height={0} 
+                        sizes="100vw"
+                        className="h-auto w-[110px]"
+                        style={{
+                            objectFit: 'contain'
+                        }}
+                    />
+                </div>
+                <div className="title bg-accent rounded-b-xl py-4">
+                    <h3 className="text-center text-sm font-semibold">{category.name}</h3>
+                </div>
             </div>
-            <div className="title bg-accent rounded-b-xl py-4">
-                <h3 className="text-center text-sm font-semibold">{category.name}</h3>
-            </div>
-        </div>
         </Link>
      );
 }
--- src/app/catalog/components/CategoryList.tsx
+++ src/app/catalog/components/CategoryList.tsx
@@ -4,7 +4,7 @@ import CategoryItem from "./CategoryItem";
 const CategoryList = async () => {
     const categories = await prismaClient.category.findMany();
     return ( 
-        <div className="flex flex-wrap gap-8 justify-start mb-4">
+        <div className="flex flex-wrap gap-4 sm:gap-8 justify-start mb-4">
             {
                 categories.map((category) => (
                     <CategoryItem key={category.id} category={category} />
